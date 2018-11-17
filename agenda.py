e7579297d0a44d6bf3013452ef2939623b0d1c66
Anderson <a.lucassilvadeoliveira@gmail.com>
2018-11-17 18:07:06 -0200

    Atualizado: 17/11/18 180:07

--- agenda.py
+++ agenda.py
@@ -1,21 +1,32 @@
 # preciso arrumar uma forma de armazenar os dados.
 agenda = dict()
+
 def insert(dc):
     while True:
 
         data = input('insira a data de hoje!')
         dc[data] = input('Pode inserir seus compromissos/metas:')
-        x = input('deseja continuar? [S/N]').upper()[0]
+        x = input('deseja continuar inserindo? [S/N]').upper()[0]
         if x == "N":
             break
 
 
 def remove():
     print('caso não saiba a data, insira 0 para mostrar a agenda!')
+
     while True:
         x = input('quer remover algo certo?, vamos lá: insira a data da nota.')
-        if agenda[x] in agenda.keys():
+        if x in agenda.keys():
             del agenda[x]
+            print('Nota removida com sucesso!')
+        else:
+            print('Nota inexistente, favor repetir o processo.')
+
+        funcao = input('Quer continuar ? [S/N]').upper()[0]
+        if x == 0:
+            mostrartudo(agenda)
+
+        if funcao == 'N':
             break
 
 def boasvindas():
@@ -32,24 +43,75 @@ def boasvindas():
 
         print('Senha incorreta.')
 
+def mostrartudo(dc):
 
+    print(dc)
+
+
+
+def mostrar(dc):
+    while True:
+        print('Caso não saiba a data, insira "0" para mostrar tudo.')
+
+        x = input('insira a data da nota:')
+
+        if x == 0:
+            mostrartudo(agenda)
+        else:
+            print(f'{dc[x]}')
+
+        funcao = input('Deseja acessar algo mais? [S/N]:').upper()[0]
+
+        if funcao == 'N':
+            break
 
 boasvindas()
 
 while True:
+    acao = ''
+    print('insira "ajuda" para mais informações:')
+    funcao = input('insira o que quer fazer:')
 
-    funcao = input('o que deseja fazer anderson:? (help = mostrar comandos)')
-    if 'inserir' in funcao:
-        insert(agenda)
-        break
-    while True:
-        s = input('deseja continuar? [S/N]').upper()[0]
-
-        if s == "S":
-            funcao = 0
+    if funcao == 'ajuda':
+        print("mostrar =  mostra uma notas.\n inserir = insere uma nota nova.\n remover = remove uma nota. \n modificar = modifica uma nota. \n mostrartudo = mostra todas as notas")
+    else:
+        if funcao == 'inserir':
+            insert(agenda)
+            funcao = 'x'
         else:
-            break
-#voltar aos comandos! preciso resolver isso para testar o comando de remoção!
+            if funcao == 'remover':
+                remove()
+            else:
+                if funcao == 'mostrartudo':
+                    mostrartudo(agenda)
+                else:
+                    if funcao == 'mostrar':
+                        mostrar(agenda)
+
+
+
+
+
+
+
+
+    acao = input('Quer fazer mais algo? [S/N]').upper()[0]
+
+    if acao  == 'N':
+        print('Volte sempre <3')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
