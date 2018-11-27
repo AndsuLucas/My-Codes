5f7ad8e2c27e5582a6384a0db1a67a712d4212c4
Anderson <a.lucassilvadeoliveira@gmail.com>
2018-11-27 07:45:06 -0200

    modified:

--- agenda.py
+++ agenda.py
@@ -1,131 +1,97 @@
-# preciso arrumar uma forma de armazenar os dados.
-agenda = dict()
-
-def insert(dc):
-    while True:
-
-        data = input('insira a data de hoje!')
-        dc[data] = input('Pode inserir seus compromissos/metas:')
-        x = input('deseja continuar inserindo? [S/N]').upper()[0]
-        if x == "N":
-            break
+def ajuda():
+    print('Acessar,Resetar,Escrever,Mostrar,Sair:')
 
 
-def remove():
-    print('caso não saiba a data, insira 0 para mostrar a agenda!')
-
-    while True:
-        x = input('quer remover algo certo?, vamos lá: insira a data da nota.')
-        if x in agenda.keys():
-            del agenda[x]
-            print('Nota removida com sucesso!')
-        else:
-            print('Nota inexistente, favor repetir o processo.')
-
-        funcao = input('Quer continuar ? [S/N]').upper()[0]
-        if x == 0:
-            mostrartudo(agenda)
-
-        if funcao == 'N':
-            break
-
-def boasvindas():
-
-    print('Bem vindo Anderson!')
-    print('--------'*30)
-    while True:
-        senha = int(123)
-        validacao = int(input('Insira sua senha:'))
-        print('------------'*30)
-
-        if validacao == senha:
-            break
-
-        print('Senha incorreta.')
-
-def mostrartudo(dc):
-
-    print(dc)
+def sep():
+    print('-'*30)
 
 
+class Agenda(object):
 
-def mostrar(dc):
-    while True:
-        print('Caso não saiba a data, insira "0" para mostrar tudo.')
+    def __init__(self):
 
-        x = input('insira a data da nota:')
+        self.cont = 0
+        self.agenda  = open('agenda.txt', 'r')
+        print(self.agenda.read())
+        self.agenda.close()
 
-        if x == 0:
-            mostrartudo(agenda)
-        else:
-            print(f'{dc[x]}')
+    def escrever(self):
 
-        funcao = input('Deseja acessar algo mais? [S/N]:').upper()[0]
+        self.agenda = open('agenda.txt', 'a')
+        self.data = input('Insira a data:')
+        self.conteudo = input('Insira o conteúdo:')
 
-        if funcao == 'N':
-            break
+        self.agenda.write(f'\n{self.data}: {self.conteudo}')
 
-boasvindas()
+        self.agenda.close()
 
+    def acessar(self):
+
+        self.agenda = open('agenda.txt', 'r')
+        dx = input('insira a data:')
+        sep()
+        for d in self.agenda:
+
+            if dx in d:
+
+                self.cont += 1
+
+                sep()
+                print(f'Conteúdo encontrado:\n{d}')
+                sep()
+
+        print(f'{self.cont} Notas encontradas.')
+        self.agenda.close()
+
+    def resetar(self):
+
+         while True:
+
+            funcao = input('Deseja mesmo resetar a agenda? [S/N]').upper()[0]
+
+            if funcao == "N" or funcao == "S":
+                break
+         if funcao == 'S':
+            self.agenda = open('agenda.txt', 'w')
+            self.agenda.close()
+    def mostrar(self):
+
+        self.agenda = open('agenda.txt', 'r')
+        print(self.agenda.read())
+        self.agenda.close()
+
+#estruturando a agenda no terminal
+a = Agenda()
+sep()
+ajuda()
+sep()
 while True:
-    acao = ''
-    print('insira "ajuda" para mais informações:')
-    funcao = input('insira o que quer fazer:')
 
-    if funcao == 'ajuda':
-        print("mostrar =  mostra uma notas.\n inserir = insere uma nota nova.\n remover = remove uma nota. \n modificar = modifica uma nota. \n mostrartudo = mostra todas as notas")
-    else:
-        if funcao == 'inserir':
-            insert(agenda)
-            funcao = 'x'
-        else:
-            if funcao == 'remover':
-                remove()
-            else:
-                if funcao == 'mostrartudo':
-                    mostrartudo(agenda)
-                else:
-                    if funcao == 'mostrar':
-                        mostrar(agenda)
+    f = input('insira o que deseja fazer:').lower()
 
+    if (f == 'acessar'):
 
+        a.acessar()
 
+    elif(f == 'escrever'):
 
+        a.escrever()
 
+    elif(f == 'mostrar'):
 
+        a.mostrar()
 
+    elif(f == 'resetar'):
 
-    acao = input('Quer fazer mais algo? [S/N]').upper()[0]
-
-    if acao  == 'N':
-        print('Volte sempre <3')
+        a.resetar()
+    elif(f == 'sair'):
         break
+    
+    else:
+        print('Função inválida')
 
+    pergunta = input('\nQuer continuar? [N/S]').upper()[0]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (pergunta == 'N'):
+        break
 
