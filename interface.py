a99b0a85946a4dcb6db7e7b7f4f5ae25dcaceadb
Anderson Lucas <44175834+AndsuLucas@users.noreply.github.com>
2019-01-25 21:33:19 -0200

    Add files via upload
    Projeto de agenda refatorado com CRUD MYSQL

---
+++ interface.py
@@ -0,0 +1,9 @@
+from bancodedados import (
+	Agenda
+)
+agenda = Agenda()
+#agenda.criar_registro()
+#agenda.salvar_alteracoes()
+#agenda.consultar()
+agenda.excluir_registro()
+agenda.salvar_alteracoes()
