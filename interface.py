7f4ab2380eaff7871aa7823e850ab52568c74ac8
Anderson Lucas <44175834+AndsuLucas@users.noreply.github.com>
2019-01-25 21:34:18 -0200

    Delete interface.py

--- interface.py
+++
@@ -1,9 +0,0 @@
-from bancodedados import (
-	Agenda
-)
-agenda = Agenda()
-#agenda.criar_registro()
-#agenda.salvar_alteracoes()
-#agenda.consultar()
-agenda.excluir_registro()
-agenda.salvar_alteracoes()
